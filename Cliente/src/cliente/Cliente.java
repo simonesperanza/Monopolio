599f448329943d855d0b12aaa5cdf553ab2a100a
Simon Esperanza <simonesperanza3@gmail.com>
2018-06-03 15:44:02 -0400

    Clases para probar conexion

--- Cliente/src/cliente/Cliente.java
+++ Cliente/src/cliente/Cliente.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cliente;
 
-/**
- *
- * @author simon
- */
 public class Cliente {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Conexion con = new Conexion();
+        con.initClient();
+
     }
-    
+
 }
