f515098c53c60c6c9aaf08356cb318e4330a2c45
LexaFdz <alexander.fada@gmail.com>
2018-06-24 08:14:05 -0400

    Agregados efectos de botones y logica basica de mensajes a enviar a servidor en el cliente

--- Cliente/src/Home.java
+++ Cliente/src/Home.java
@@ -67,6 +67,9 @@ public class Home extends javax.swing.JFrame {
         BotonComprar = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        BotonVender = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
         Propiedades = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
@@ -164,7 +167,7 @@ public class Home extends javax.swing.JFrame {
                         .addComponent(PropiedadesCantidad)
                         .addGap(27, 27, 27)
                         .addComponent(jLabel14)))
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         PerfilLayout.setVerticalGroup(
             PerfilLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -192,21 +195,36 @@ public class Home extends javax.swing.JFrame {
         Acciones.setBackground(new java.awt.Color(255, 255, 255));
 
         BotonDados.setBackground(new java.awt.Color(235, 236, 238));
+        BotonDados.setPreferredSize(new java.awt.Dimension(162, 103));
+        BotonDados.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BotonDadosMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                BotonDadosMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                BotonDadosMousePressed(evt);
+            }
+        });
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8_Dice_52px.png"))); // NOI18N
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8_Dice_52px.png"))); // NOI18N
+        jLabel2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(51, 102, 255));
         jLabel3.setText("LANZAR!");
+        jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
         javax.swing.GroupLayout BotonDadosLayout = new javax.swing.GroupLayout(BotonDados);
         BotonDados.setLayout(BotonDadosLayout);
         BotonDadosLayout.setHorizontalGroup(
             BotonDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonDadosLayout.createSequentialGroup()
-                .addContainerGap(34, Short.MAX_VALUE)
+                .addContainerGap(46, Short.MAX_VALUE)
                 .addGroup(BotonDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonDadosLayout.createSequentialGroup()
                         .addComponent(jLabel2)
@@ -215,7 +233,7 @@ public class Home extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonDadosLayout.createSequentialGroup()
                         .addComponent(jLabel3)
                         .addGap(27, 27, 27)))
-                .addGap(31, 31, 31))
+                .addGap(30, 30, 30))
         );
         BotonDadosLayout.setVerticalGroup(
             BotonDadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -230,6 +248,18 @@ public class Home extends javax.swing.JFrame {
         );
 
         BotonComprar.setBackground(new java.awt.Color(235, 236, 238));
+        BotonComprar.setPreferredSize(new java.awt.Dimension(162, 51));
+        BotonComprar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BotonComprarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                BotonComprarMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                BotonComprarMousePressed(evt);
+            }
+        });
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8_E-commerce_30px.png"))); // NOI18N
 
@@ -253,9 +283,50 @@ public class Home extends javax.swing.JFrame {
             .addGroup(BotonComprarLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(BotonComprarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
                     .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING))
-                .addGap(0, 10, Short.MAX_VALUE))
+                .addGap(0, 5, Short.MAX_VALUE))
+        );
+
+        BotonVender.setBackground(new java.awt.Color(235, 236, 238));
+        BotonVender.setPreferredSize(new java.awt.Dimension(162, 51));
+        BotonVender.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BotonVenderMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                BotonVenderMouseExited(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                BotonVenderMousePressed(evt);
+            }
+        });
+
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/icons8_Apartment_25px.png"))); // NOI18N
+
+        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(51, 102, 255));
+        jLabel8.setText("VENDER");
+
+        javax.swing.GroupLayout BotonVenderLayout = new javax.swing.GroupLayout(BotonVender);
+        BotonVender.setLayout(BotonVenderLayout);
+        BotonVenderLayout.setHorizontalGroup(
+            BotonVenderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BotonVenderLayout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jLabel7)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel8)
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
+        BotonVenderLayout.setVerticalGroup(
+            BotonVenderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonVenderLayout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(BotonVenderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.LEADING))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout AccionesLayout = new javax.swing.GroupLayout(Acciones);
@@ -264,19 +335,24 @@ public class Home extends javax.swing.JFrame {
             AccionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, AccionesLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(BotonComprar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(BotonDados, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGroup(AccionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(BotonComprar, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BotonVender, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(BotonDados, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         AccionesLayout.setVerticalGroup(
             AccionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(AccionesLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(AccionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(BotonDados, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(BotonComprar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(BotonDados, javax.swing.GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+                    .addGroup(AccionesLayout.createSequentialGroup()
+                        .addComponent(BotonComprar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BotonVender, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         Propiedades.setBackground(new java.awt.Color(255, 255, 255));
@@ -317,10 +393,10 @@ public class Home extends javax.swing.JFrame {
             ControlesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(ControlesLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(ControlesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(Propiedades, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(Acciones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(ControlesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Acciones, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Propiedades, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
             .addComponent(Perfil, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         ControlesLayout.setVerticalGroup(
@@ -330,7 +406,7 @@ public class Home extends javax.swing.JFrame {
                 .addComponent(Perfil, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(Propiedades, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 284, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 279, Short.MAX_VALUE)
                 .addComponent(Acciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
@@ -339,6 +415,82 @@ public class Home extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    /*
+    * Metodos para poner color/fondo a los botones en sus distintos estados.
+    */
+    public void setColor(JPanel panel)
+    {
+        panel.setBackground(new java.awt.Color(197, 197, 197));
+    }
+     
+    public void setColor2(JPanel panel)
+    {
+        panel.setBackground(new java.awt.Color(135, 135, 135));
+    }
+
+    public void resetColor(JPanel panel)
+    {
+        panel.setBackground(new java.awt.Color(240,240,240));
+    }
+    private void BotonDadosMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonDadosMouseExited
+        // TODO add your handling code here:
+        resetColor(BotonDados);
+    }//GEN-LAST:event_BotonDadosMouseExited
+
+    private void BotonDadosMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonDadosMouseEntered
+        // TODO add your handling code here:
+        setColor(BotonDados);
+    }//GEN-LAST:event_BotonDadosMouseEntered
+
+    private void BotonDadosMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonDadosMousePressed
+        // TODO add your handling code here:
+        setColor2(BotonDados);
+        try {
+            _out.writeInt(2);       
+        } catch (IOException ex) {
+            Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // pedir la posicion en que queda el usuario
+        // pedir el balance/cash
+    }//GEN-LAST:event_BotonDadosMousePressed
+
+    private void BotonComprarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonComprarMouseEntered
+        // TODO add your handling code here:
+        setColor(BotonComprar);
+    }//GEN-LAST:event_BotonComprarMouseEntered
+
+    private void BotonComprarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonComprarMouseExited
+        // TODO add your handling code here:
+        resetColor(BotonComprar);
+    }//GEN-LAST:event_BotonComprarMouseExited
+
+    // NO FUNCIONA AUN, arreglar
+    /*public void DoSomethingAlpha(Jugador jug) {
+      jug.getSaldo();
+    }*/
+    
+    private void BotonComprarMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonComprarMousePressed
+        // TODO add your handling code here:
+        setColor2(BotonComprar);
+        
+    }//GEN-LAST:event_BotonComprarMousePressed
+
+    private void BotonVenderMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonVenderMouseEntered
+        // TODO add your handling code here:
+        setColor(BotonVender);
+    }//GEN-LAST:event_BotonVenderMouseEntered
+
+    private void BotonVenderMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonVenderMouseExited
+        // TODO add your handling code here:
+        resetColor(BotonVender);
+    }//GEN-LAST:event_BotonVenderMouseExited
+
+    private void BotonVenderMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonVenderMousePressed
+        // TODO add your handling code here:
+        setColor2(BotonVender);
+        
+    }//GEN-LAST:event_BotonVenderMousePressed
 
     /**
      * @param args the command line arguments
@@ -410,6 +562,7 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JLabel Balance;
     private javax.swing.JPanel BotonComprar;
     private javax.swing.JPanel BotonDados;
+    private javax.swing.JPanel BotonVender;
     private javax.swing.JLabel Casa1;
     private javax.swing.JLabel Casa2;
     private javax.swing.JLabel Casa3;
@@ -435,6 +588,8 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
